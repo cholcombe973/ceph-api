c07c97da0483c185dfd845e04328d188bd241ade
Chris Holcombe <xfactor973@gmail.com>
2016-05-12 14:07:49 -0700

    Initial commit

---
+++ README.rst
@@ -0,0 +1,11 @@
+Ceph API
+========
+
+This project generates helper functions to interact with Ceph over JSON
+rather than using subprocess.popen, check_call, check_output, etc.  If you
+are considering importing subprocess into your project stop and use this
+library instead.  You'll save yourself many future headaches.
+
+The utility that generates these helper functions is located here:
+`Ceph Command Parser <https://github.com/cholcombe973/ceph_command_parser>`_
+
---
+++ setup.py
@@ -0,0 +1,77 @@
+__author__ = 'Chris Holcombe <chris.holcombe@canonical.com>'
+
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ceph_api',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.0',
+
+    description='A wrapper around librados json commands',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/cholcombe973/ceph_command_parser',
+
+    # Author details
+    author='Chris Holcombe',
+    author_email='chris.holcombe@canonical.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='ceph development',
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['python-cephlibs'],
+)
